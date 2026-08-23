70d75413be7209727019ad263f3ba68fb2bb4679
HiteshCode-Projects <hiteshtokasiya@gmail.com>
2026-08-23 15:00:20 +0530

    react

---
+++ React/my-first-app/src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import './App.css'
+
+//React is a functional Component - Code is written inside Function
+
+function App(){
+  
+  //Js
+
+
+  return(
+    //HTML Code
+    <div className="xyz">
+             {/* //Html Tags -In React ALL THE HTML TAGS NEEDS TO BE CLOSED*/}
+
+         <h2>Welcome To React</h2> 
+
+         <button>Login</button>  
+
+         <a href="https://www.google.com" target="_blank">Google</a> 
+
+         <input   placeholder="Enter Name"       />
+
+
+
+    </div>
+  )
+
+}
+
+export default App  //This is Function calling in React - export default functionName
